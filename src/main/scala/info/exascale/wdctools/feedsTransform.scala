690d2f5ca44bf99e9c8385a77d73e8cf5d73e64d
Victor Felder <victorfelder@gmail.com>
2016-03-14 16:23:51 +0100

    writeup consequences

--- src/main/scala/info/exascale/wdctools/feedsTransform.scala
+++ src/main/scala/info/exascale/wdctools/feedsTransform.scala
@@ -1,6 +1,7 @@
 package info.exascale.wdctools
 
 import com.netaporter.uri.Uri
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import org.apache.spark.{SparkConf, SparkContext, sql}
 import org.apache.spark.sql.functions._
 import scala.language.postfixOps
@@ -8,13 +9,30 @@ import scala.language.postfixOps
 object feedsTransform {
   def main(args: Array[String]) {
     val conf = new SparkConf()
-      .setAppName("RecreateWithHostname")
-      .set("spark.sql.parquet.compression.codec", "snappy")
+      .setAppName("TransformFeeds")
+      .set("spark.sql.parquet.compression.codec", "snappy") // snappy compression for parquet
+
     val sc = new SparkContext(conf)
-
     val sqlContext = new sql.SQLContext(sc)
-    val df = sqlContext.read.parquet("/user/vfelder/feeds/feedscoalesced.parquet/")
 
+    // schema for the CSV we'll load
+    val feedSchema = StructType(Array(
+      StructField("page", StringType, true),
+      StructField("tag", StringType, true),
+      StructField("type", StringType, true)))
+
+    // read the CSV with our schema using databricks' spark-csv
+    val df = sqlContext
+      .read
+      .format("com.databricks.spark.csv")
+      .option("header", "false")
+      .schema(feedSchema)
+      .load("/data/WDC_112015/data/feeds/*.csv.gz")
+
+
+    // extract from each page url their hostname
+    // some URLs (eg. http://example.com:/foo.html) made Uri crash, I had to implement a regex
+    // based check
     val hostnamePattern = "((\\/\\/|https\\:\\/\\/|http\\:\\/\\/)([^\\/\\:]+))"r
 
     val getHost: (String => String) = (page: String) => {
@@ -23,11 +41,11 @@ object feedsTransform {
         println(s"prefiltering failed: $page")
         ""
       } else {
-        val prefilteredString = preFiltered.get
+        val preFilteredString = preFiltered.get
         try {
-          val host = Uri.parse(prefilteredString).host
+          val host = Uri.parse(preFilteredString).host
           if (host.isEmpty) {
-            println(s"parsing failed: $page prefiltered as: $prefilteredString")
+            println(s"parsing failed: $page prefiltered as: $preFilteredString")
             ""
           } else {
             host.get
@@ -42,14 +60,17 @@ object feedsTransform {
       }
     }
 
+    // extract rel="…"
     val relPattern = """.*rel=["']?([^'"]*)["']?.*""".r
     val getRel: (String => String) = (tag: String) => {
       tag match {
+        // pattern-matching regex matches is so nice
         case relPattern(captured) => captured
         case _ => ""
       }
     }
 
+    // extract href="…"
     val hrefPattern = """.*href=["']?([^'"]*)["']?.*""".r
     val getHref: (String => String) = (tag: String) => {
       tag match {
@@ -58,6 +79,7 @@ object feedsTransform {
       }
     }
 
+    // extract title="…"
     val titlePattern = """.*title=["']?([^'"]*)["']?.*""".r
     val getTitle: (String => String) = (tag: String) => {
       tag match {
@@ -65,10 +87,13 @@ object feedsTransform {
         case _ => ""
       }
     }
-    val getLCType: (String => String) = (typeString: String) => {
-      typeString.toLowerCase
+
+    // transform to lowercase
+    val getLCType: (String => String) = (str: String) => {
+      str.toLowerCase
     }
 
+    // create spark sql user-defined functions for each of these scala function
     val sqlGetHost = udf(getHost)
     val sqlGetRel = udf(getRel)
     val sqlGetHref = udf(getHref)
@@ -76,11 +101,19 @@ object feedsTransform {
     val sqlGetLCType = udf(getLCType)
 
     df
+      // add hostname column based on page column
       .withColumn("hostname", sqlGetHost(col("page")))
+      // add rel column based on tag column
       .withColumn("rel", sqlGetRel(col("tag")))
+      // add href column based on tag column
       .withColumn("href", sqlGetHref(col("tag")))
+      // add title column based on tag column
       .withColumn("title", sqlGetTitle(col("tag")))
+      // replace type column with its lowercase version
       .withColumn("type", sqlGetLCType(col("type")))
+      // only output 1,000 parquet files from the 35k csv.gz input files
+      .coalesce(1000)
+      // write as parquet to my hdfs home folder
       .write.parquet("/user/vfelder/feeds/feedsparsed.parquet/")
   }
 }
