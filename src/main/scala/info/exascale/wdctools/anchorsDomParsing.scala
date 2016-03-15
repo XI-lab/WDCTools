90b6d0c6b4dc867b9d176931a8c46a7c18c1dada
Victor Felder <victorfelder@gmail.com>
2016-03-15 23:36:08 +0100

    Domparsing lc

--- src/main/scala/info/exascale/wdctools/anchorsDomParsing.scala
+++ src/main/scala/info/exascale/wdctools/anchorsDomParsing.scala
@@ -5,14 +5,14 @@ import java.util.regex.{Matcher, Pattern}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types.{StructField, StructType, StringType}
 import org.apache.spark.{SparkConf, SparkContext}
-import scala.xml.Node
+import scala.xml.{NodeSeq, Node}
 import scala.xml.Source.fromString
 import org.apache.spark.sql.Row
 import org.apache.spark.sql._
 
 
 object anchorsDomParsing {
-  def findParentOfText(dom: Node, value: String) = (dom \\ "p").filter(_.text.contains(value))
+  def findParentOfText(dom: Node, value: String): NodeSeq = (dom \\ "p").filter(_.text.toLowerCase.contains(value))
 
   def extractFromHtml(content: String, page: String, pattern: Pattern): Array[Row] = {
     val pageMatcher: Matcher = pattern.matcher(content)
@@ -22,15 +22,13 @@ object anchorsDomParsing {
     while (pageMatcher.find) {
       val href = pageMatcher.group(6).replace("\n", " ").replace("\r", " ").replace("\t", " ")
       val link = pageMatcher.group(1).replace("\n", " ").replace("\r", " ").replace("\t", " ")
-      val exp = pageMatcher.group(0).replace("\n", " ").replace("\r", " ").replace("\t", " ")
-      val paragraph = findParentOfText(dom, exp)
-        if (paragraph.nonEmpty) {
-          output = Row(
-            content, page,
-            page, href,
-            link, paragraph.text
-          ) :: output
-        }
+
+      val paragraph = findParentOfText(dom, href.toLowerCase)
+      if (paragraph.nonEmpty) {
+        output = Row(page, href, link, paragraph.text) :: output
+      } else {
+        output = Row(page, href, link, "") :: output
+      }
     }
 
     output.toArray
@@ -46,21 +44,20 @@ object anchorsDomParsing {
     val linkPattern = Pattern.compile("<a[^>]* href=[\\\"']?((http|\\/\\/|https){1}([^\\\"'>]){0,20}(\\.m.)?wikipedia\\.[^\\\"'>]{0,5}\\/w(iki){0,1}\\/[^\\\"'>]+)[\"']?[^>]*>(.+?)<\\/a>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL)
 
     val schema = StructType(
-        StructField("content", StringType, true) :: StructField("url", StringType, true) ::
         StructField("page", StringType, true) :: StructField("href", StringType, true) ::
         StructField("link", StringType, true) :: StructField("paragraph", StringType, true) :: Nil
       )
 
-    val rowRDD = sqlContext.read.json("/user/atonon/WDC_112015/data/anchor_pages/*.gz")
-        .toJavaRDD
+    val rdd: RDD[Row] = sqlContext.read.json("/user/atonon/WDC_112015/data/anchor_pages/*.gz")
+      .map(row => {
+        val originalColumns = row.toSeq.toList
+        extractFromHtml(originalColumns(0).toString, originalColumns(1).toString, linkPattern)
+      })
+      .flatMap(row => row)
 
-    val stillRDD: RDD[Row] = sqlContext.createDataFrame(rowRDD, schema)
-      .map(row => extractFromHtml(row.get(0).toString, row.get(1).toString, linkPattern))
-      .flatMap(rows => rows.toSeq)
-
-    val df = sqlContext.createDataFrame(stillRDD, schema)
-      .coalesce(1000)
-      .write.parquet("/user/vfelder/anchorsContext/anchors1.parquet/")
+      val df = sqlContext.createDataFrame(rdd, schema)
+        .coalesce(1000)
+        .write.parquet("/user/vfelder/anchorsContext/anchors1.parquet/")
 
   }
 }
